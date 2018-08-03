917c3f2ae84a77dcaa25a3c6c3ca1e1242df6db4
deanantonic <dean_antonic@hotmail.com>
2018-08-03 15:37:01 +0200

    q5 done

---
+++ zhiwehu/q5.py
@@ -0,0 +1,29 @@
+"""
+Question 5
+Level 1
+
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+
+Hints:
+Use __init__ method to construct some parameters
+"""
+
+class Foo:
+
+    def __init__(self):
+        self.input = raw_input("Input some characters:")
+
+    def getString(self):
+        return self.input
+
+    def printString(self):
+        return self.input.upper()
+
+
+test = Foo()
+print test.getString()
+print test.printString()
