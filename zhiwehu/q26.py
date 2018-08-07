51b82046afd013879a3d1562c09ba95b515e6880
deanantonic <dean_antonic@hotmail.com>
2018-08-07 18:24:09 +0200

    q26 done

---
+++ zhiwehu/q26.py
@@ -0,0 +1,13 @@
+"""
+Question:
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
+"""
+
+
+def sum_num(a, b):
+    return a + b
+
+print sum_num(3, 4)
