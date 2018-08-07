a9efa9c90d2f0b3087e69fc3419bcb2365c5a843
deanantonic <dean_antonic@hotmail.com>
2018-08-07 18:45:18 +0200

    q27 done

---
+++ zhiwehu/q27.py
@@ -0,0 +1,15 @@
+"""
+Question:
+Define a function that can convert a integer into a string and print it in console.
+
+Hints:
+
+Use str() to convert a number to string.
+"""
+
+
+def print_number(n):
+    print str(n)
+
+
+print_number(56)
