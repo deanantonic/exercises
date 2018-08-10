7fbdfb9603f6c7ca1c2f631188d74ffc539e307b
deanantonic <dean_antonic@hotmail.com>
2018-08-10 13:55:49 +0200

    q81 done

---
+++ zhiwehu/q81.py
@@ -0,0 +1,15 @@
+"""
+Question:
+
+Please write a program to shuffle and print the list [3,6,7,8].
+
+
+
+Hints:
+Use shuffle() function to shuffle a list.
+"""
+from random import shuffle
+
+someList = [3, 6, 7, 8]
+shuffle(someList)
+print someList
