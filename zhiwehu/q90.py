0957fac0f1266c545c942afe41c1695f27b7d354
deanantonic <dean_antonic@hotmail.com>
2018-08-10 15:08:23 +0200

    q90 done

--- zhiwehu/q90.py
+++ zhiwehu/q90.py
@@ -4,5 +4,19 @@ With a given list [12,24,35,24,88,120,155,88,120,155], write a program to print 
 Hints:
 Use set() to store a number of values without duplicate.
 """
+# my favorite solution, needs more_itertools library
+from more_itertools import unique_everseen
 nums = [12, 24, 35, 24, 88, 120, 155, 88, 120, 155]
-print sorted(set(nums), key=nums.index)
+print list(unique_everseen(nums))
+
+
+
+# this is an interesting solution but only for this example, can't rely on it
+# print sorted(set(nums), key=nums.index)
+
+
+# alternative solution
+# from collections import OrderedDict
+# nums = [12, 24, 35, 24, 88, 120, 155, 88, 120, 155]
+# unique = list(OrderedDict.fromkeys(nums))
+# print unique
