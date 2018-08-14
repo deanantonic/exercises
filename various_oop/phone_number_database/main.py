d0d7051e82b57b65b6b6942410203a803b8648fd
deanantonic <dean_antonic@hotmail.com>
2018-08-14 17:18:53 +0200

    description

--- various_oop/phone_number_database/main.py
+++ various_oop/phone_number_database/main.py
@@ -1,3 +1,39 @@
+"""
+Phone number database
+Description
+
+In this exercise, you should write a simple application that can help you search for the owner of a phone number.
+User stories (required)
+
+    As a User, I'd like to enter a full phone number (with non-correct punctuations), to get the name of the owner of that number.
+    As an Administrator, I'd like to change the phone number database without writing into Python code
+
+Extra feature's user story (optional)
+
+    As a User, I'd like to enter the beginning of a phone number (with non-correct punctuations) to get a list of possible owners
+
+Example output
+
+user@computer:$ python phone_numbers.py ./phone_data_10000.csv
+Please enter the phone number: 046162276
+This number belongs to: Ka Sturdy
+
+user@computer:$ python phone_numbers.py ./phone_data_10000.csv
+Please enter the phone number: 999999
+No match found.
+
+user@computer:$ python phone_numbers.py
+No database file was given.
+
+What the application should do?
+
+    Read the .csv file (given first parameter)
+    Create Person instances from each name-number pair
+    Store the Person objects in a list
+    Ask the user for a phone number
+    Find the owner of the given phone number
+    Print out the result of the search
+"""
 import sys
 import os
 from person import Person
