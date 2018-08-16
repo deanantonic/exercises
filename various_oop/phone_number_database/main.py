17a4c0c6c88d7fc72cbefd054d045b79406a3fdf
deanantonic <dean_antonic@hotmail.com>
2018-08-16 11:41:18 +0200

    Squashed commit of the following:
    commit 91d94b7e21ce9f0a33b5ec5795713a617dc3f2aa
    Author: deanantonic <dean_antonic@hotmail.com>
    Date:   Thu Aug 16 07:42:30 2018 +0200
    
        hundred doors task done
    
    commit 20b55de40a1c89fa4f40156d7f1894f61b07f34f
    Author: deanantonic <dean_antonic@hotmail.com>
    Date:   Wed Aug 15 12:12:11 2018 +0200
    
        added optional user story, returns a list of possible owners
    
    commit 6f5ef69a8ec72e84dada28b85ec25d0097573bec
    Author: deanantonic <dean_antonic@hotmail.com>
    Date:   Tue Aug 14 20:33:09 2018 +0200
    
        second user story finished, admin can edit db
    
    commit d0d7051e82b57b65b6b6942410203a803b8648fd
    Author: deanantonic <dean_antonic@hotmail.com>
    Date:   Tue Aug 14 17:18:53 2018 +0200
    
        description
    
    commit a619e73a59e490a208a964d8542e027275b7f549
    Author: deanantonic <dean_antonic@hotmail.com>
    Date:   Tue Aug 14 17:12:39 2018 +0200
    
        housekeeping

---
+++ various_oop/phone_number_database/main.py
@@ -0,0 +1,162 @@
+"""
+Phone number database
+Description
+
+In this exercise, you should write a simple application that can help you search for the owner of a phone number.
+User stories (required)
+
+    As a User, I'd like to enter a full phone number (with non-correct punctuations), to get the name of the owner of that number.
+    As an Administrator, I'd like to change the phone number database without writing into Python code
+
+Extra feature's user story (optional)
+
+    As a User, I'd like to enter the beginning of a phone number (with non-correct punctuations) to get a list of possible owners
+
+Example output
+
+user@computer:$ python phone_numbers.py ./phone_data_10000.csv
+Please enter the phone number: 046162276
+This number belongs to: Ka Sturdy
+
+user@computer:$ python phone_numbers.py ./phone_data_10000.csv
+Please enter the phone number: 999999
+No match found.
+
+user@computer:$ python phone_numbers.py
+No database file was given.
+
+What the application should do?
+
+    Read the .csv file (given first parameter)
+    Create Person instances from each name-number pair
+    Store the Person objects in a list
+    Ask the user for a phone number
+    Find the owner of the given phone number
+    Print out the result of the search
+"""
+import sys
+import os
+import configparser
+from person import Person
+parentDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# __file__ is the full path to where the script you are running is located.
+os.sys.path.insert(0, parentDir)
+# run this on the command line with "python various_oop/main.py phone_data_10000.csv"
+
+
+def open_csv(file_name):
+    with open(file_name, 'r') as f:
+        return [i.rstrip() for i in f]
+
+
+def edit_csv(file_name, record):
+    with open(file_name, 'a') as f:
+        f.write(record)
+
+
+def get_csv_file_name(argv_list):
+    return None if len(argv_list) <= 1 else argv_list[1]
+
+
+def format_output(person):
+    if isinstance(person, Person):
+        # this if clause handles some tests
+        return "This number belongs to: {}".format(person._name)
+    elif isinstance(person, list):
+        return "Incomplete number, possible owners:\n{}".format("\n".join(person))
+    elif isinstance(person, str):
+        return "This number belongs to: {}".format(person)
+    else:
+        return "No match found."
+
+
+def get_person_by_phone_number(person_list, user_input_phone_number):
+    possibleRecords = []
+    normalizedNumber = Person.normalize_phone_number(user_input_phone_number)
+    phoneNumberLength = len(normalizedNumber)
+    for person in person_list:
+        if isinstance(person, Person):
+            # this if clause handles some tests
+            if normalizedNumber == Person.normalize_phone_number(person._phone_number):
+                return person
+        elif normalizedNumber == Person.normalize_phone_number(person.split(",")[1]):
+            return person
+        else:
+            if Person.normalize_phone_number(person.split(",")[1])[:phoneNumberLength] == normalizedNumber:
+                possibleRecords.append(person)
+    return possibleRecords if possibleRecords else None
+
+
+def getConfiguration():
+    parser = configparser.ConfigParser()
+    parser.optionxform = str
+    parser.read("{}{}".format(parentDir, '/phone_number_database/auth.cfg'))
+    return parser
+
+
+def getFileName():
+    file_name = get_csv_file_name(sys.argv)
+    if file_name is None:
+        print('No database file was given.')
+        sys.exit(0)
+
+    file_relpath = '/phone_number_database/{}'.format(file_name)
+    file_name = "{}{}".format(parentDir, file_relpath)
+    return file_name
+
+
+def adminExec(admin):
+    config = getConfiguration()
+    # TODO salt and hash password
+    if admin != config.get("admin", "pass"):
+        print "Wrong administrator password. This attempt has been logged!"
+        return
+
+    def welcome():
+        action = raw_input("Welcome back Administrator! Would you like to edit or read database records (e-edit, r-read, press enter for exit): ")
+        if not action or action not in ("r", "e"):
+            return
+
+        if action == "r":
+            main()
+            return
+        elif action == "e":
+            # TODO check record conforms to format or record already in database
+
+            while True:
+                record = raw_input("Enter full name and phone number separating them with a comma e.g. John Doe,0356-5456-323, press enter for exit: ")
+                if not record:
+                    return
+
+                record += "\n"
+                edit_csv(getFileName(), record)
+                print "Record successfully added!"
+                moreRecords = raw_input("Would you like to edit more records Yes or No: ")
+                if not moreRecords or moreRecords.lower() in ("no", "n"):
+                    print "Finished editing database."
+                    return
+
+    welcome()
+
+
+def main():
+    person_list = open_csv(getFileName())
+    user_input_phone_number = raw_input('Please enter the phone number, press enter for exit: ')
+    if not user_input_phone_number:
+        print "No phone number provided."
+        return
+
+    match_person = get_person_by_phone_number(person_list, user_input_phone_number)
+    if isinstance(match_person, str):
+        match_person = match_person.split(",")[0]
+
+    print(format_output(match_person))
+
+
+if __name__ == '__main__':
+    admin = raw_input("Enter administrator password or press enter for guest user: ")
+    if not admin:
+        print "Welcome Guest user!"
+        main()
+    else:
+        adminExec(admin)
