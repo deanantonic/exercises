d4a09dede8d43abd0756f70732b6b791be3e1fcd
deanantonic <dean_antonic@hotmail.com>
2018-09-08 11:03:18 +0200

    the ship teams

---
+++ checkio/the_ship_teams.py
@@ -0,0 +1,39 @@
+"""
+ You have to divide all your crew members into 2 teams with the next rules: those who are elder than 40 y.o. or younger than 20, should be on the first ship and all the others - on the second. It will helps the young sailors gain the experience of the elder collegues. The input data will be the dictionary where keys will be the surnames of the sailors and the values will be their ages. After the crew formating, you should sort all of the sailors in the alphabetical order in the each list of surnames.
+
+Input: Dictionary with the sailors and their ages.
+
+Output: List of the lists with 2 teams.
+
+Precondition:
+1 <= amount of the sailors <= 100
+"""
+def two_teams(sailors):
+    ship1 = sorted([key for key in sailors if sailors[key] < 20 or sailors[key] > 40])
+    ship2 = sorted([key for key in sailors if key not in ship1])
+    return [ship1, ship2]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(two_teams({'Smith': 34, 'Wesson': 22, 'Coleman': 45, 'Abrahams': 19}))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert two_teams({
+        'Smith': 34,
+        'Wesson': 22,
+        'Coleman': 45,
+        'Abrahams': 19}) == [
+            ['Abrahams', 'Coleman'],
+            ['Smith', 'Wesson']
+            ]
+
+    assert two_teams({
+        'Fernandes': 18,
+        'Johnson': 22,
+        'Kale': 41,
+        'McCortney': 54}) == [
+            ['Fernandes', 'Kale', 'McCortney'],
+            ['Johnson']
+            ]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
